382f7167994f75a776cc8064406775ed93de6f33
Kidus Mekonen <kidusabebe1921@gmail.com>
2023-06-09 13:18:59 +0530

    added the rest of the channels

--- telegram_scraper.py
+++ telegram_scraper.py
@@ -13,42 +13,43 @@ def save_image_from_bytes(image_bytes, filename):
 def scrape_telegram_channel(api_id, api_hash, phone_number, channel_username, limit):
     # Create a TelegramClient object and authenticate
     client = TelegramClient('session_name', api_id, api_hash)
-    client.connect()
-    if not client.is_user_authorized():
-        client.send_code_request(phone_number)
-        client.sign_in(phone_number, input('Enter the code: '))
+    for name in channel_username:
+        print(name)
+        client.connect()
+        if not client.is_user_authorized():
+            client.send_code_request(phone_number)
+            client.sign_in(phone_number, input('Enter the code: '))
 
-    # Get the channel entity
-    entity = client.get_entity(channel_username)
+        # Get the channel entity
+        entity = client.get_entity(name)
 
-    # Retrieve the messages from the channel
-    messages = client.get_messages(entity, limit=limit)
+        # Retrieve the messages from the channel
+        messages = client.get_messages(entity, limit=limit)
 
-    data = []
-    for message in messages:
-        text = message.text
-        date = message.date
-        image_bytes = None
-        image_path = None
+        data = []
+        for message in messages:
+            text = message.text
+            date = message.date
+            image_bytes = None
+            image_path = None
 
-        if isinstance(message.media, MessageMediaPhoto):
-            photo = message.photo
-            if photo is not None:
-                image_bytes = client.download_media(photo, bytes)
-                if image_bytes is not None:
-                    image_path = f'images/{message.id}.jpg'  # Assuming you want to save the images in a folder named "images"
-                    save_image_from_bytes(image_bytes, image_path)
+            if isinstance(message.media, MessageMediaPhoto):
+                photo = message.photo
+                if photo is not None:
+                    image_bytes = client.download_media(photo, bytes)
+                    if image_bytes is not None:
+                        image_path = f'images/{message.id}.jpg'  # Assuming you want to save the images in a folder named "images"
+                        save_image_from_bytes(image_bytes, image_path)
 
-        data.append({'Text': text, 'Date': date, 'ImagePath': image_path})
-
+            data.append({'Text': text, 'Date': date, 'ImagePath': image_path})
     return data
 
 # Specify your API credentials and other parameters
 api_id = '29549426'
 api_hash = '44bd5a957eefe65197fe22275a65dc30'
 phone_number = '918984937192'
-channel_username = 'tikvahethiopia'
-limit = 10
+channel_username = ['tikvahethiopia','fanatelevision','ethio_mereja','Esat_tv1']
+limit = 5
 
 # Scrape the Telegram channel
 scraped_data = scrape_telegram_channel(api_id, api_hash, phone_number, channel_username, limit)
